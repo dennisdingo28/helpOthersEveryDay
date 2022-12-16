98711f64062420615ade4cc1a18f24ec8a5e9c07
Joker 281 <dennismoldovan32@gmail.com>
2022-12-16 20:28:01 +0200

    navbar + hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,23 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "lightBlue":"#2570d1",
+        "softGray":"#f0f0f0",
+      },
+      fontFamily:{
+        'Raleway':['Raleway', 'sans-serif'],
+        'DMsans':['DM Sans', 'sans-serif'],
+      }
+    },
     screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+      'xs':"468px",
+      'sm':"576px",
+      'md':"768px",
+      "lg":"992px",
+      "xl":"1200px",
+      "xxl":"1400px"
     },
   },
   plugins: [],
